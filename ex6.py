248378a0e82158084ecd5ff1d93f5999ce8a7b3a
Najurk <39627652+Najurk@users.noreply.github.com>
2018-05-25 10:32:06 -0500

    Create ex6.py

---
+++ ex6.py
@@ -0,0 +1,14 @@
+# This program will take a numerical grade and give a letter grade output
+grade = int(input("Enter your grade: "))
+if grade >= 90:
+  print("A")
+elif grade >= 80:
+  print("B")
+elif grade >= 70:
+  print("C")
+elif grade >= 60:
+  print("D")
+elif grade >= 50:
+  print("F")
+  
+  

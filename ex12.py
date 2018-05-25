97c01ab00528711b44414938f2103b54d298f37f
Najurk <39627652+Najurk@users.noreply.github.com>
2018-05-25 10:35:19 -0500

    Create ex12.py

---
+++ ex12.py
@@ -0,0 +1,8 @@
+# Create a blank array
+num_array = list()
+num = input("Enter how many elements you want:")
+print ('Enter numbers in array: ')
+for i in range(int(num)):
+    n = input("num :")
+    num_array.append(int(n))
+print ('ARRAY: ',num_array)

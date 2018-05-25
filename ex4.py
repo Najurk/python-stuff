efee3f5f4a2cdff57931c3c36b6cb2ec2906ffdc
Najurk <39627652+Najurk@users.noreply.github.com>
2018-05-25 10:30:57 -0500

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,13 @@
+# Read 2 integers:
+
+
+# Check to see which one is bigger
+
+
+# print the bigger integer
+a = str(input()) 
+b = str(input())
+if a>b:
+  print("The bigger number is " + a)
+else:
+  print ("The bigger number is " + b)

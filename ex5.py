8d6baf197cd4a0fd0b0eec6b83321bbcb8ac3f92
Najurk <39627652+Najurk@users.noreply.github.com>
2018-05-25 10:31:27 -0500

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,9 @@
+# This is an array that contains different foods
+food_items = ["turkey","ham", "spam","eggs","nuts"]
+print(food_items[3])
+# print "eggs" from the loop USING an Array
+
+print("This will print the array of food items")
+# Use a for loop to print all of the food items
+for x in food_items:
+  print(x)

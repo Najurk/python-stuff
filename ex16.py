ed7b844a07b6fc31e37d064b82a362b15845ffa7
Najurk <39627652+Najurk@users.noreply.github.com>
2018-05-25 10:37:18 -0500

    Create ex16.py

---
+++ ex16.py
@@ -0,0 +1,15 @@
+# Read a string:
+
+a= (input())
+
+# Print a string:
+
+print (a[2]) #1
+print(a.strip()[-2]) #2
+print (a[:5]) #3
+print (a[:-2]) #4
+print(a[::2]) #5
+print(a[1::2]) #6
+print(a[::-1]) #7
+print(a[::-2])
+print(len(a))

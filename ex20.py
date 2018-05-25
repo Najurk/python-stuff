8a484ca6aeb48abc0f5de7c173f3f9d5309dc3c0
Najurk <39627652+Najurk@users.noreply.github.com>
2018-05-25 10:39:51 -0500

    Create ex20.py

---
+++ ex20.py
@@ -0,0 +1,31 @@
+print('How old do you think you are in seconds in Earth years?')
+x = int(input())
+print (x/31557600)
+
+print('How old do you think you are in seconds in Mercury years?')
+x = int(input())
+print (x/7600000)
+
+print('How old do you think you are in seconds in Venus years?')
+x = int(input())
+print (x/19400000)
+
+print('How old do you think you are in seconds in Mars years?')
+x = int(input())
+print (x/59400000)
+
+print('How old do you think you are in seconds in Jupiter years?')
+x = int(input())
+print (x/370000000)
+
+print('How old do you think you are in seconds in Saturn years?')
+x = int(input())
+print (x/930000000)
+
+print('How old do you think you are in seconds in Uranus years?')
+x= int(input())
+print (x/2700000000)
+
+print('How old do you think you are in seconds in Neptune years?')
+x= int(input())
+print (x/5200000000)
